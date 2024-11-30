3bd0e7a9129b6e440b064cb8d1c7334ab78220e2
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-30 16:58:40 +0530

    API serving JSON responses

--- drf/quickstart/views.py
+++ drf/quickstart/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from quickstart.models import Quickstart
+from quickstart.serializers import QuickstartSerializer
 
 
 @csrf_exempt
@@ -24,3 +25,47 @@ def snippet_list(request):
 			serializer.save()
 			return JsonResponse(serializer.data, status=201)
 		return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+	#retrieve, update or delete a code snippet
+
+	try:
+		print('snippet wale tr mai gaya')
+		snippet = Quickstart.objects.get(pk=pk)
+	except Quickstart.DoesNotExist:
+		print('does not exist wale except mai gaya')
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		print('get mai gaya')
+		serializer = QuickstartSerializer(snippet)
+		return JsonResponse(serializer.data)
+	
+	elif request.method == 'PUT':
+		print('put mai gaya')
+		#put method is used when you want to save or replace everything in a resource.
+		#it will update all existing fields of the database or create a new one if not existings
+		data = JSONParser().parse(request)
+		serializer = QuickstartSerializer(snippet, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=400)
+	
+	elif request.method == 'DELETE':
+		print('delete mai gya')
+		#delete the resource from the server
+		snippet.delete()
+		return HttpResponse(status=204)
+
+
+
+
+
+
+
+
+
+
