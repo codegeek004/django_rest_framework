e99d546dccf9b1c6739c18c925968fb73da206aa
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-01 11:37:10 +0530

    authentication added in api

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quickstart.urls')),
 ]   
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
--- drf/quickstart/views.py
+++ drf/quickstart/views.py
@@ -1,24 +1,29 @@
 from django.contrib.auth.models import User
 from quickstart.models import Quickstart
 from quickstart.serializers import QuickstartSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
+from django.views.decorators.csrf import csrf_protect
+
 
 #most consise code. Other approaches are in the views_tutorial file.
+# @csrf_protect
 class SnippetList(generics.ListCreateAPIView):
 	queryset = Quickstart.objects.all()
 	serializer_class = QuickstartSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
-
+# @csrf_protect
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Quickstart.objects.all()
 	serializer_class = QuickstartSerializer	
-
+# @csrf_protect
 class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-
+# @csrf_protect
 class UserDetail(generics.RetrieveAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
