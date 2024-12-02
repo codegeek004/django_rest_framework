7cb91227ce0525c15c98a7b6a79f51c4d2adab9e
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-02 17:02:39 +0530

    implemented viewsets

--- drf/quickstart/serializers_approaches.py
+++ drf/quickstart/serializers_approaches.py
@@ -1,37 +1,3 @@
-from rest_framework import serializers
-from quickstart.models import Quickstart, LANGUAGE_CHOICES, STYLE_CHOICES
-from django.contrib.auth.models import User 
-############Hyperlinked Serializer##############
-# The HyperlinkedModelSerializer has the following differences from ModelSerializer:
-# It does not include the id field by default.
-# It includes a url field, using HyperlinkedIdentityField.
-# Relationships use HyperlinkedRelatedField, instead of PrimaryKeyRelatedField.
-
-
-##hyperlinks are URL references used to represent relationships between resources in an API. 
-#They provide a more RESTful approach by linking related entities through their URLs rather
-# than embedding data directly or using primary keys.
-
-class QuickstartSerailizer(serializers.HyperlinkedModelSerializer):
-	owner = serializers.ReadOnlyField(source='owner.username')
-	highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
-
-	class Meta:
-		model=Quickstart
-		fields = ['url','id','highlight','owner',
-				  'title', 'code', 'linenos', 'language', 'style']
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-	snippets = serializers.HyperlinkedIdentityField(many=True, view_name='snippet-detail')
-
-	class Meta:
-		model = User 
-		fields = ['url','id','username','snippets']
-
-
-
-
-
 #This was working with primary key
 #Modelserializer is simply shortcut for creating serializer class.
 # class QuickstartSerializer(serializers.ModelSerializer):
--- drf/quickstart/views.py
+++ drf/quickstart/views.py
@@ -1,36 +1,74 @@
 from django.contrib.auth.models import User
 from quickstart.models import Quickstart
 from quickstart.serializers import QuickstartSerializer, UserSerializer
-from rest_framework import generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from quickstart.permissions import IsOwnerOrReadOnly
-
-
 #most consise code. Other approaches are in the views_tutorial file.
-# @csrf_protect
-class SnippetList(generics.ListCreateAPIView):
-	queryset = Quickstart.objects.all()
-	serializer_class = QuickstartSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-	def perform_create(self, serializer):
-		serializer.save(owner=self.request.user)
+class SnippetViewList(viewsets.ModelViewSet):
+
+    # This ViewSet automatically provides `list`, `create`, `retrieve`,
+    # `update` and `destroy` actions.
+
+    # Additionally we will also provide an extra `highlight` action.
+
+    queryset = Quickstart.objects.all()
+    serializer_class = QuickstartSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+    	snippet = self.get_object()
+    	return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+    	serializer.save(owner=self.request.user)
+ 
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Quickstart.objects.all()
-	serializer_class = QuickstartSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]	
 
-class UserList(generics.ListAPIView):
+# class SnippetList(generics.ListCreateAPIView):
+# 	queryset = Quickstart.objects.all()
+# 	serializer_class = QuickstartSerializer
+# 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# 	def perform_create(self, serializer):
+# 		serializer.save(owner=self.request.user)
+
+
+# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+# 	queryset = Quickstart.objects.all()
+# 	serializer_class = QuickstartSerializer
+# 	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+# #creating endpoint for highlighted snippets
+# class SnippetHighlight(generics.GenericAPIView):
+# 	queryset = Quickstart.objects.all()
+# 	renderer_class = [renderers.StaticHTMLRenderer]
+
+# 	def get(self, request, *args, **kwargs):
+# 		snippet = self.get_object()
+# 		return Response(snippet.highlighted)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	#This viewset automatically provides list and retrieve actions
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
+
+
+#Replaced this 2 classes with single viewset
+# class UserList(generics.ListAPIView):
+# 	queryset = User.objects.all()
+# 	serializer_class = UserSerializer
+
+# class UserDetail(generics.RetrieveAPIView):
+# 	queryset = User.objects.all()
+# 	serializer_class = UserSerializer
 
 
 #Creating endpoint for the root of our api(entry point or base of the url)
@@ -43,14 +81,6 @@ def api_root(request, format=None):
 		})
 
 
-#creating endpoint for highlighted snippets
-class SnippetHighlight(generics.GenericAPIView):
-	queryset = Quickstart.objects.all()
-	renderer_class = [renderers.StaticHTMLRenderer]
-
-	def get(self, request, *args, **kwargs):
-		snippet = self.get_object()
-		return Response(snippet.highlighted)
 
 
 
