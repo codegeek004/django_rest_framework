31b00c7c110e19cb4ebfb960cdf162b3467e47a6
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-02 15:16:26 +0530

    added end points for the root and highlighted snippet

--- drf/quickstart/views.py
+++ drf/quickstart/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from quickstart.models import Quickstart
 from quickstart.serializers import QuickstartSerializer, UserSerializer
-from rest_framework import generics, permissions
-from django.views.decorators.csrf import csrf_protect
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from quickstart.permissions import IsOwnerOrReadOnly
 
 
@@ -13,19 +15,56 @@ class SnippetList(generics.ListCreateAPIView):
 	serializer_class = QuickstartSerializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
-# @csrf_protect
+
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Quickstart.objects.all()
 	serializer_class = QuickstartSerializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]	
-# @csrf_protect
+
 class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-# @csrf_protect
+
 class UserDetail(generics.RetrieveAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
+
+#Creating endpoint for the root of our api(entry point or base of the url)
+@api_view
+def api_root(request, format=None):
+	#reverse redirects you to the url dynamically.
+	return Response({
+		'users':reverse('user-list', request=request, format=format),
+		'snippets':reverse('snippet-list', request=request, format=format)
+		})
+
+
+#creating endpoint for highlighted snippets
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Quickstart.objects.all()
+	renderer_class = [renderers.StaticHTMLRenderer]
+
+	def get(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
