e770e66127ff4eaa5480c9b30a89675546ce0639
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-02 17:14:11 +0530

    binded viewsets to urls explicitly

--- drf/quickstart/urls.py
+++ drf/quickstart/urls.py
@@ -1,6 +1,32 @@
 from django.urls import path 
 from . import views
+from rest_framework import renderers
+from quickstart.views import api_root, SnippetViewSet, UserViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
+
+snippet_list = SnippetViewSet.as_view({
+    'get':'list',
+    'post':'create',
+    })
+
+snippet_detail = SnippetViewSet.as_view({
+    'get':'retrieve',
+    'put':'update',
+    'patch':'partial_update',
+    'delete':'destroy'
+    })
+
+snippet_highlight = SnippetViewSet.as_view({
+    'get':'highlight'  
+    }, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get':'list'
+    })
+
+user_detail = UserViewSet.as_view({
+    'get':'retrieve'
+    })
 
 urlpatterns = [
     path('', views.api_root),
